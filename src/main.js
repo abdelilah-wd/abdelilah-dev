60bbd35b075e7cd9b6fd87a66a3bf77b378d34f2
AbdeLilah-Dev <contactme.abdelilah@gmail.com>
2025-09-18 13:52:48 +0100

    updated

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import { Dropdown, Collapse } from 'bootstrap'
 import './style.css'
 import './styles/component.css' ;
 import './styles/animations.css' ;
 import './components/particles.js';
-import { animate } from 'animejs';
 import { startTyping } from './components/typing_animation.js'
+emailjs.init('m44VsGGpNL77DBF17');
 
 // Initialize when DOM is ready
 
@@ -45,13 +44,12 @@ let contact_section = document.querySelector(".contact-section")
 let all_sections = [home_section, about_section, work_section, contact_section];
 window.addEventListener("scroll", event => {
   all_sections.forEach((ele, index) => {
-    console.log(window.scrollY);
-    if (window.scrollY > 600) {
+    if (window.scrollY > 400) {
       header_section.classList.add("fixed");
     } else {
       header_section.classList.remove("fixed");
     }
-    let offsetTop = ele.offsetTop - 20;
+    let offsetTop = ele.offsetTop - 200;
     let offsetBottom = ele.offsetTop + ele.offsetHeight;
     if (offsetTop < window.scrollY && offsetBottom > window.scrollY){
       menuLinks.forEach((ele) => ele.classList.remove("active"));
@@ -59,3 +57,35 @@ window.addEventListener("scroll", event => {
     }
   })
 })
+
+let contact_form_inputs = document.querySelectorAll(".contact-form div .client-input");
+
+contact_form_inputs.forEach(ele => {
+  console.log(ele);
+  let ele_label = document.querySelector(".contact-form div> label[for=\"" + ele.getAttribute("id") + "\"]");
+  ele.addEventListener("blur", event => {
+    console.log(ele.value);
+    console.log(ele_label);
+    if (ele.value) {
+      ele_label.classList.add("active");
+    } else {
+      ele_label.classList.remove("active");
+    }
+  })
+})
+
+let send_email = document.querySelector(".send-email");
+document.querySelector(".contact-form .form").addEventListener("submit", event => {
+  console.log("hello world\n");
+  event.preventDefault();
+  emailjs.sendForm('service_vtmwzob', 'template_mz6d1go', '#contact-form')
+    .then(function() {
+      alert('Message sent successfully!');
+    }, function(error) {
+      alert('Failed to send message: ' + error.text);
+    });
+})
+
+
+let footerTime = document.querySelector(".footer-section .time");
+footerTime.innerHTML = ((new Date).getFullYear() == 2025 ? "" : "2025 - ") +(new Date).getFullYear();
